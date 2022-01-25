e9bb98a201ab74d0e9eab2647e8bbcb0e37fa744
GitHub Action <action@github.com>
2022-01-25 11:19:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/burtbai/stringformatting/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/burtbai/stringformatting/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.burtbai.stringformatting.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.burtbai.stringformatting.MyBundle
 
 class MyApplicationService {
 
